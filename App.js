c9d2621935052320a5ed720c84c083de1b61b57b
shava2c <Agsh0505@gmail.com>
2021-03-06 14:39:51 +0900

    アイコンの追加

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { Ionicons, FontAwesome } from '@expo/vector-icons'
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +10,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  itemContainer: {
+    height: 70,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  qaContainer: {
+    backgroundColor: 'blue',
+    flex: 1,
+    padding: 16,
+    justifyContent: 'space-between',
+  },
+  checkontainer: {
+    width: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.qaContainer}>
+          <View></View>
+          <View></View>
+        </View>
+        <View style={styles.checkontainer}>
+          <FontAwesome size={50} color="gray" name="check-square"></FontAwesome>
+        </View>
+      </View>
+    </View>
+  )
+}
