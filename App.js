f7f48903dbb9bbe7007bd22ef1199a1f3e9a7bb3
shava2c <Agsh0505@gmail.com>
2021-03-06 15:36:27 +0900

    リファクタリング

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
+import ListItem from './component/ListItem'
 
 const styles = StyleSheet.create({
   container: {
@@ -44,39 +45,7 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.qaContainer}>
-          <View style={styles.detailContainer}>
-            <FontAwesome
-              style={styles.qaIcon}
-              size={20}
-              name="question-circle-o"
-            ></FontAwesome>
-            <Text numberOfLines={2} style={styles.text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam,
-            </Text>
-          </View>
-          <View style={styles.detailContainer}>
-            <FontAwesome
-              style={styles.qaIcon}
-              size={20}
-              name="comment-o"
-            ></FontAwesome>
-            <Text numberOfLines={2} style={styles.text}>
-              これが答え
-            </Text>
-          </View>
-        </View>
-        <View style={styles.checkontainer}>
-          <FontAwesome
-            size={50}
-            color="green"
-            name="check-square"
-          ></FontAwesome>
-        </View>
-      </View>
+      <ListItem />
     </View>
   )
 }
