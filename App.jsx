7a73d9498315316af08eb63c2b2672e2584bc207
shava2c <Agsh0505@gmail.com>
2021-03-08 12:37:47 +0900

    ナビゲーターの追加

--- App.jsx
+++ App.jsx
@@ -1,60 +1,6 @@
 import React from 'react'
-import { StyleSheet, SafeAreaView, FlatList } from 'react-native'
-import ListItem from './component/ListItem'
-import articles from './dummy/articles.json'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? 25 : 0,
-  },
-  itemContainer: {
-    height: 70,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: 'row',
-  },
-  qaContainer: {
-    flex: 1,
-    justifyContent: 'space-between',
-    padding: 1,
-  },
-  detailContainer: {
-    height: 32,
-    flexDirection: 'row',
-  },
-  qaIcon: {
-    width: 30,
-    padding: 5,
-    color: 'black',
-  },
-  checkontainer: {
-    width: 70,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    width: '90%',
-    fontSize: 12,
-  },
-})
+import AppNavigator from './navigation/AppNavigator'
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            qText={item.qText}
-            aText={item.aText}
-            checkFlg={item.checkFlg}
-          />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </SafeAreaView>
-  )
+  return <AppNavigator />
 }
